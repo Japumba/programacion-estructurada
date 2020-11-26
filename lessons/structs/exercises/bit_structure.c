626a53cd17c162399fbc64d083ef56798a8a9da2
japumba <jdelamorav@hotmail.com>
2020-11-26 06:51:17 -0600

    add fields to bit structure exercise

--- lessons/structs/exercises/bit_structure.c
+++ lessons/structs/exercises/bit_structure.c
@@ -9,25 +9,62 @@
  * la memoria indicada en User, manteniendo la información de la estructura.
  * 
  * INSTRUCCIONES:
+ * 1. Compila y ejecuta el codigo. Observa el tamaño actual de la estructura User.
+ * 
+ * 2. Modifica la estructura, usando campos de bits, para que no pase del tamaño deseado
+ * 
+ * 3. 
  */
 
-#define MAX_DESIRED_STRUCT_SIZE 1
+#define MAX_DESIRED_STRUCT_SIZE 5
 
-typedef struct {
-  unsigned char es_admin; // 0 = false, 1 = true
+enum ESTATUS_ACADEMICO
+{
+  NO_INSCRITO,
+  ACTIVO,
+  SUSPENDIDO,
+  DADO_DE_BAJA
+};
+
+typedef struct
+{
+  unsigned char es_admin;        // 0 = false, 1 = true
   unsigned char departamento_id; // 0 = DESI, 1 = DPTI, 2 = DMAF, 3 = DIA, 4 = DEAM, 5 = DFIH
-  unsigned char estatus_academico; // 0 = NO INSCRITO, 1 = ACTIVO, 2 = SUSPENDIDO, 3 = DADO DE BAJA
+  unsigned char estatus_academico;
   unsigned char tiene_acceso_estacionamiento; // 0 = false, 1 = true
-  unsigned char tiene_acceso_laboratorios; // 0 = false, 1 = true
+  unsigned char tiene_acceso_laboratorios;    // 0 = false, 1 = true
+  unsigned int matricula;
 } User;
+
+void printUser(User u);
 
 int main()
 {
+  User user1 = {
+      0, // false
+      2, // DMAF
+      ACTIVO,
+      0, // false
+      1, // true
+      677549};
+
+  printUser(user1);
+
   /* validate struct size for exercise */
   unsigned long size = sizeof(User);
 
-  if(size > MAX_DESIRED_STRUCT_SIZE){
-    printf("Error en el ejercicio!\nDebe ser un tamano menor a %i byte, pero es de %lu bytes.", MAX_DESIRED_STRUCT_SIZE, size);
+  if (size <= MAX_DESIRED_STRUCT_SIZE)
+  {
+    printf("\nExcelente!\nTu estructura tiene %lu byte(s), no pasa de %i byte(s).", size, MAX_DESIRED_STRUCT_SIZE);
+  }
+  else
+  {
+    printf("\nError en el ejercicio!\nDebe ser un tamano menor a %i byte, pero es de %lu bytes.", MAX_DESIRED_STRUCT_SIZE, size);
     exit(-1);
   }
+}
+
+void printUser(User u)
+{
+  printf("usuario [%u]\n", u.matricula);
 }
